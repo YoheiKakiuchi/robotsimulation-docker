81b614322abf62e6239fd65bed1b93886dbf57da
leus <eus@jsk.t.u-tokyo.ac.jp>
2017-05-24 11:30:56 +0900

    [pointgrey_docker] pass ROS_IP and ROS_HOSTNAME

--- pointgrey_docker/run.sh
+++ pointgrey_docker/run.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 
-docker run -it --rm --privileged --net=host -w="/home/pointgrey" yoheikakiuchi/pointgrey_test "$@"
+source $HOME/ros/indigo/devel/setup.bash
+rossetip
+
+docker run -it --rm --privileged \
+    --net=host \
+    -w="/home/pointgrey" \
+    --net="host" \
+    --env="ROS_IP" \
+    --env="ROS_HOSTNAME" \
+    yoheikakiuchi/pointgrey_test "$@"
