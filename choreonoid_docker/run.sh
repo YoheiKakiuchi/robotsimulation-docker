2d47c891b9ea098b7fcd23f2400095b7e68588db
YoheiKakiuchi <youhei@jsk.imi.i.u-tokyo.ac.jp>
2017-05-24 12:55:37 +0900

    [choreonoid_docker] fix entrypoint

--- choreonoid_docker/run.sh
+++ choreonoid_docker/run.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 nvidia-docker run -it \
+    --name="choreonoid_simulation" \
     --env="DISPLAY" \
     --env="QT_X11_NO_MITSHM=1" \
     --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
     -w="/home/choreonoid" \
-    rosnvidia
+    rosnvidia "$@"
