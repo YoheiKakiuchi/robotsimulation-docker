0b18eeb24db632e85e1810254c63591ca8a1d80b
YoheiKakiuchi <youhei@jsk.imi.i.u-tokyo.ac.jp>
2017-05-26 13:12:20 +0900

    [choreonoid_docker] update run.sh

--- choreonoid_docker/run.sh
+++ choreonoid_docker/run.sh
@@ -1,15 +1,20 @@
 #!/bin/bash
 
-#DOCKER=docker
-DOCKER=nvidia-docker
+RUNDOC=${DOCKER:-nvidia-docker}
+DEFAULT_USER_DIR="/tmp"
+VAR=${@:-"rtmlaunch hrpsys_choreonoid_tutorials jaxon_jvrc_choreonoid.launch"}
 
-### ???
-${DOCKER} rm choreonoid_simulation
+xhost +local:root
 
-${DOCKER} run -it \
+${RUNDOC} rm choreonoid_simulation
+
+${RUNDOC} run -it \
     --name="choreonoid_simulation" \
     --env="DISPLAY" \
     --env="QT_X11_NO_MITSHM=1" \
     --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
+    --volume="${PROG_DIR:-$DEFAULT_USER_DIR}:/home/choreonoid/user_programs" \
     -w="/home/choreonoid" \
-    choreonoidsim "$@"
+    choreonoidsim ${VAR}
+
+xhost -local:root
