881c6c4cac9740dc733714b6756ef602f4f8a1ad
YoheiKakiuchi <youhei@jsk.imi.i.u-tokyo.ac.jp>
2017-07-27 00:06:39 +0900

    add openrave files

--- choreonoid_docker/my_entrypoint.sh
+++ choreonoid_docker/my_entrypoint.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
-set -e
+## set -e
 
 # setup ros environment
 #source "/opt/ros/$ROS_DISTRO/setup.bash"
 source "${WORKHOME}/catkin_ws/devel/setup.bash"
+
+export PATH=`rospack find openrave`/bin:$PATH
+export PYTHONPATH=/opt/ros/indigo/bin:`openrave-config --python-dir`:$PYTHONPATH
+export OPENRAVE_HOME=/home/choreonoid/user_programs
 
 export ROS_IP=$(hostname -i)
 export ROS_HOSTNAME=$(hostname -i)
