77af4cb51940d3ee0b7e701874649430c44d0207
Yohei Kakiuchi <youhei@jsk.imi.i.u-tokyo.ac.jp>
2019-01-24 14:08:15 +0900

    fix run.sh

--- choreonoid_docker/run.sh
+++ choreonoid_docker/run.sh
@@ -1,16 +1,15 @@
 #!/bin/bash
 
 OPT=${DOCKER_OPTION} ## -it --cpuset-cpus 0-2
-iname=${DOCKER_IMAGE:-"jsk_private/trans_robot:16.04"} ## name of image (should be same as in build.sh)
-cname=${DOCKER_CONTAINER:-"trans_robot_container"} ## name of container (should be same as in exec.sh)
+iname=${DOCKER_IMAGE:-"yoheikakiuchi/choreonoidsim:16.04"} ## name of image (should be same as in build.sh)
+cname=${DOCKER_CONTAINER:-"choreonoidsim"} ## name of container (should be same as in exec.sh)
 
 DEFAULT_USER_DIR="$(pwd)"
 
 #VAR=${@:-"bash"}
 #VAR=${@:-"rtmlaunch hrpsys_choreonoid_tutorials jaxon_jvrc_choreonoid.launch"}
-#VAR=${@:-"rtmlaunch hrpsys_choreonoid_tutorials jaxon_jvrc_choreonoid.launch LOAD_OBJECTS:=true ENVIRONMENT_YAML:=/home/choreonoid/catkin_ws/src/rtmros_choreonoid/hrpsys_choreonoid_tutorials/config/footsal.yaml"}
-VAR=${@:-"rtmlaunch hrpsys_choreonoid_tutorials jaxon_jvrc_choreonoid.launch LOAD_OBJECTS:=true ENVIRONMENT_YAML:=/home/choreonoid/user_programs/footsal.yaml"}
-
+#VAR=${@:-"rtmlaunch hrpsys_choreonoid_tutorials jaxon_jvrc_choreonoid.launch LOAD_OBJECTS:=true ENVIRONMENT_YAML:=/choreonoid/catkin_ws/src/rtmros_choreonoid/hrpsys_choreonoid_tutorials/config/footsal.yaml"}
+VAR=${@:-"rtmlaunch hrpsys_choreonoid_tutorials jaxon_jvrc_choreonoid.launch LOAD_OBJECTS:=true ENVIRONMENT_YAML:=/userdir/footsal.yaml"}
 
 NET_OPT="--net=host --env=ROS_IP --env=ROS_HOSTNAME --env=DOCKER_CLIENT_IP"
 
@@ -24,6 +23,7 @@ docker run ${OPT}    \
     --runtime=nvidia \
     ${NET_OPT}       \
     --env="DISPLAY"  \
+    --env="ROBOT=JAXON_RED" \
     --env="QT_X11_NO_MITSHM=1" \
     --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
     --name=${cname} \
