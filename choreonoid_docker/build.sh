eec9ee1f56450d5fc60301f063125d0cb3c9fc1d
Yohei Kakiuchi <youhei@jsk.imi.i.u-tokyo.ac.jp>
2019-04-19 21:01:43 +0900

    [choreonoid_docker] add options to build.sh

--- choreonoid_docker/build.sh
+++ choreonoid_docker/build.sh
@@ -1,15 +1,28 @@
 #!/bin/bash
 set -e
 
-DOCKER_USER=yoheikakiuchi
-BUILD_CMD=docker
+####
+#
+# BUILD_NAME=yourname ./build.sh
+# USE_IMAGE=no ./build.sh ## build all images (not using image in dockerhub)
+# BUILD=echo ./build.sh   ## not build just making Dockerfile
+#
+####
 
-#ROS_VERSION=kinetic
-UBUNTU_VERSION=16.04 ## 18.04(not tested)
-CHOREONOID_VERSION=latest
-#CHOREONOID_VERSION=release-1.6 ## latest
+DOCKER_USER=${BUILD_NAME:-yoheikakiuchi}
+BUILD_CMD=${BUILD:-docker}
 
-USE_IMAGE_IN_DOCKERHUB=yes
+### ubuntu version ###
+#UBUNTU_VERSION=18.04 ## (ROS melodic) not tested
+UBUNTU_VERSION=16.04  ## (ROS kinetic) recomended
+#UBUNTU_VERSION=16.04_no_gl ## for not using nvidia docker2
+
+### choreonoid version ###
+#CHOREONOID_VERSION=latest ##
+CHOREONOID_VERSION=release-1.6 ## recomended
+
+###
+USE_IMAGE_IN_DOCKERHUB=${USE_IMAGE:-"yes"}
 if [ ${USE_IMAGE_IN_DOCKERHUB} == "yes" ]; then
     DOCKER_USER=yoheikakiuchi
     OLD_BUILD_CMD=${BUILD_CMD}
@@ -20,6 +33,8 @@ fi
 if [ "$UBUNTU_VERSION" == 18.04 ]; then
     ### not tested
     ${BUILD_CMD} build -f ../ros_gl/Dockerfile.ros_gl.melodic --tag=${DOCKER_USER}/ros_gl:${UBUNTU_VERSION} .
+elif [ "$UBUNTU_VERSION" == "16.04_no_gl" ]; then
+    ${BUILD_CMD} build -f ../ros_gl/Dockerfile.ros_no_gl --tag=${DOCKER_USER}/ros_gl:${UBUNTU_VERSION} .
 else
     ${BUILD_CMD} build -f ../ros_gl/Dockerfile.ros_gl --tag=${DOCKER_USER}/ros_gl:${UBUNTU_VERSION} .
 fi
@@ -41,7 +56,7 @@ sed -e "s/@CHOREONOID_VERSION@/${CHOREONOID_VERSION}/" -e "s/@UBUNTU_VERSION@/${
 ${BUILD_CMD} build -f Dockerfile.hrpsys.${UBUNTU_VERSION}_${CHOREONOID_VERSION} \
        --tag=${DOCKER_USER}/hrpsys:${UBUNTU_VERSION}_${CHOREONOID_VERSION} .
 
-##
+
 if [ ${USE_IMAGE_IN_DOCKERHUB} == "yes" ]; then
     DOCKER_USER=yoheikakiuchi
     BUILD_CMD=${OLD_BUILD_CMD}
