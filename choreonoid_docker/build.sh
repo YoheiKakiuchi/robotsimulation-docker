61e142fdb4445086d99a3dd5a0bb82d865101225
Yohei Kakiuchi <youhei@jsk.imi.i.u-tokyo.ac.jp>
2019-05-15 23:22:26 +0900

    update to choreonoid:release-1.7

--- choreonoid_docker/build.sh
+++ choreonoid_docker/build.sh
@@ -18,8 +18,9 @@ UBUNTU_VERSION=16.04  ## (ROS kinetic) recomended
 #UBUNTU_VERSION=16.04_no_gl ## for not using nvidia docker2
 
 ### choreonoid version ###
-#CHOREONOID_VERSION=latest ##
-CHOREONOID_VERSION=release-1.6 ## recomended
+#CHOREONOID_VERSION=master ##
+#CHOREONOID_VERSION=release-1.6 ## recomended
+CHOREONOID_VERSION=release-1.7
 
 ###
 USE_IMAGE_IN_DOCKERHUB=${USE_IMAGE:-"yes"}
@@ -42,13 +43,15 @@ else
 fi
 
 ### choreonoid
-if [ "$CHOREONOID_VERSION" == latest ]; then
-    ### todo
-    sed -e "s/@UBUNTU_VERSION@/${UBUNTU_VERSION}/" -e "s/@NAME_SPACE@/${DOCKER_USER}/" \
-	Dockerfile.choreonoid.latest.in > Dockerfile.choreonoid.${UBUNTU_VERSION}_${CHOREONOID_VERSION}
+if [ "$CHOREONOID_VERSION" == release-1.6 ]; then
+    ### not use python3
+    sed -e "s/@CHOREONOID_VERSION@/${CHOREONOID_VERSION}/" -e "s/@UBUNTU_VERSION@/${UBUNTU_VERSION}/" -e "s/@NAME_SPACE@/${DOCKER_USER}/" \
+	-e "s/@EXTRA_CNOID_CMAKE_OPTIONS@/-DUSE_PYTHON3=OFF -DUSE_PYBIND11=OFF/" \
+        Dockerfile.choreonoid.new.in > Dockerfile.choreonoid.${UBUNTU_VERSION}_${CHOREONOID_VERSION}
 else
     sed -e "s/@CHOREONOID_VERSION@/${CHOREONOID_VERSION}/" -e "s/@UBUNTU_VERSION@/${UBUNTU_VERSION}/" -e "s/@NAME_SPACE@/${DOCKER_USER}/" \
-        Dockerfile.choreonoid.in > Dockerfile.choreonoid.${UBUNTU_VERSION}_${CHOREONOID_VERSION}
+	-e "s/@EXTRA_CNOID_CMAKE_OPTIONS@//" \
+        Dockerfile.choreonoid.new.in > Dockerfile.choreonoid.${UBUNTU_VERSION}_${CHOREONOID_VERSION}
 fi
 ${BUILD_CMD} build -f Dockerfile.choreonoid.${UBUNTU_VERSION}_${CHOREONOID_VERSION} \
        --tag=${DOCKER_USER}/choreonoid:${UBUNTU_VERSION}_${CHOREONOID_VERSION} .
