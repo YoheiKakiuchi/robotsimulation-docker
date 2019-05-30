b469fe2ca87a1e5f8aa2a18de4661ec376b9a0ce
Yohei Kakiuchi <youhei@jsk.imi.i.u-tokyo.ac.jp>
2019-05-30 18:36:55 +0900

    [gazebo_docker] fix for using DockerHub auto_build

--- gazebo_docker/exec.sh
+++ gazebo_docker/exec.sh
@@ -1,16 +1,15 @@
 #!/bin/bash
 
 OPT=${DOCKER_OPTION} ## -it --cpuset-cpus 0-2
-cname=${DOCKER_CONTAINER:-"aerosim"} ## name of container (should be same as in run.sh)
+cname=${DOCKER_CONTAINER:-"aero_gazebo"} ## name of container (should be same as in run.sh)
 
-VAR=${@:-"bash"}
-
-NET_OPT="--env=ROS_IP --env=ROS_HOSTNAME --env=DOCKER_CLIENT_IP"
+VAR=${@:-"bash --rcfile /my_entryrc"}
+if [ $# -eq 0 -a -z "$OPT" ]; then
+    OPT=-it
+fi
 
 docker exec ${OPT}          \
        --privileged         \
-       --runtime=nvidia     \
-       ${NET_OPT}           \
        --env="DISPLAY"      \
        --env="QT_X11_NO_MITSHM=1" \
        --workdir="/userdir" \
--- gazebo_docker/run.sh
+++ gazebo_docker/run.sh
@@ -1,19 +1,25 @@
 #!/bin/bash
 
 OPT=${DOCKER_OPTION} ## -it --cpuset-cpus 0-2
-iname=${DOCKER_IMAGE:-"yoheikakiuchi/aero-ros-pkg:18.04"} ## name of image (should be same as in build.sh)
-cname=${DOCKER_CONTAINER:-"aerosim"} ## name of container (should be same as in exec.sh)
+iname=${DOCKER_IMAGE:-"yoheikakiuchi/aero-ros-pkg:kinetic"} ## name of image (should be same as in build.sh)
+cname=${DOCKER_CONTAINER:-"aero_gazebo"} ## name of container (should be same as in exec.sh)
 
 DEFAULT_USER_DIR="$(pwd)"
 
-#VAR=${@:-"bash"}
+#VAR=${@:-"bash --rcfile /my_entryrc"}
 VAR=${@:-"roslaunch aero_gazebo aero_gazebo.launch"}
 
-if [ "$DOCKER_CLIENT_IP" == "" ]; then
-#    export DOCKER_CLIENT_IP=127.0.0.1
-    export DOCKER_CLIENT_IP=localhost
+## --net=mynetworkname
+## docker inspect -f '{{.NetworkSettings.Networks.mynetworkname.IPAddress}}' choreonoidsim
+## docker inspect -f '{{.NetworkSettings.Networks.mynetworkname.Gateway}}'   choreonoidsim
+
+if [ "$DOCKER_ROS_IP" == "" ]; then
+#    export DOCKER_ROS_IP=127.0.0.1
+    export DOCKER_ROS_IP=localhost
 fi
-NET_OPT="--net=host --env=ROS_IP --env=ROS_HOSTNAME --env=DOCKER_CLIENT_IP"
+
+NET_OPT="--net=host --env=DOCKER_ROS_IP --env=DOCKER_ROS_MASTER_URI"
+#NET_OPT="--net=host --env=NVIDIA_DRIVER_CAPABILITIES --env=NVIDIA_VISIBLE_DEVICES"
 
 ##xhost +local:root
 xhost +si:localuser:root
@@ -24,6 +30,7 @@ docker run ${OPT}    \
     --privileged     \
     --runtime=nvidia \
     ${NET_OPT}       \
+    --env="DOCKER_ROS_SETUP=/catkin_ws/devel/setup.bash" \
     --env="DISPLAY"  \
     --env="QT_X11_NO_MITSHM=1" \
     --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
