7cf23244a676df9605e705e451e41bcc9375497a
Yohei Kakiuchi <youhei@jsk.imi.i.u-tokyo.ac.jp>
2019-05-30 15:07:54 +0900

    [choreonoid_docker] add --pull and --no-cache

--- choreonoid_docker/build.sh
+++ choreonoid_docker/build.sh
@@ -11,6 +11,7 @@ set -e
 
 CURRENT_NAME=${BUILD_NAME:-yoheikakiuchi}
 BUILD_CMD=${BUILD:-docker}
+DOCKER_BUILD_OPTION='--no-cache'
 
 ### ubuntu version ###
 #UBUNTU_VERSION=18.04 ## (ROS melodic) not tested
@@ -35,11 +36,11 @@ fi
 ### opengl
 if [ "$UBUNTU_VERSION" == 18.04 ]; then
     ### not tested
-    ${BUILD_CMD} build -f ../ros_gl/Dockerfile.ros_gl.melodic --tag=${DOCKER_USER}/ros_gl:${UBUNTU_VERSION} .
+    ${BUILD_CMD} build --pull ${DOCKER_BUILD_OPTION} -f ../ros_gl/Dockerfile.ros_gl.melodic --tag=${DOCKER_USER}/ros_gl:${UBUNTU_VERSION} .
 elif [ "$UBUNTU_VERSION" == "16.04_no_gl" ]; then
-    ${BUILD_CMD} build -f ../ros_gl/Dockerfile.ros_no_gl --tag=${DOCKER_USER}/ros_gl:${UBUNTU_VERSION} .
+    ${BUILD_CMD} build --pull ${DOCKER_BUILD_OPTION} -f ../ros_gl/Dockerfile.ros_no_gl --tag=${DOCKER_USER}/ros_gl:${UBUNTU_VERSION} .
 else
-    ${BUILD_CMD} build -f ../ros_gl/Dockerfile.ros_gl --tag=${DOCKER_USER}/ros_gl:${UBUNTU_VERSION} .
+    ${BUILD_CMD} build --pull ${DOCKER_BUILD_OPTION} -f ../ros_gl/Dockerfile.ros_gl --tag=${DOCKER_USER}/ros_gl:${UBUNTU_VERSION} .
 fi
 
 ### choreonoid
@@ -53,13 +54,13 @@ else
         -e "s/@EXTRA_CNOID_CMAKE_OPTIONS@/-DUSE_PYTHON3=OFF/" \
         Dockerfile.choreonoid.new.in > Dockerfile.choreonoid.${UBUNTU_VERSION}_${CHOREONOID_VERSION}
 fi
-${BUILD_CMD} build -f Dockerfile.choreonoid.${UBUNTU_VERSION}_${CHOREONOID_VERSION} \
+${BUILD_CMD} build ${DOCKER_BUILD_OPTION} -f Dockerfile.choreonoid.${UBUNTU_VERSION}_${CHOREONOID_VERSION} \
        --tag=${DOCKER_USER}/choreonoid:${UBUNTU_VERSION}_${CHOREONOID_VERSION} .
 
 ### hrpsys
 sed -e "s/@CHOREONOID_VERSION@/${CHOREONOID_VERSION}/" -e "s/@UBUNTU_VERSION@/${UBUNTU_VERSION}/" -e "s/@NAME_SPACE@/${DOCKER_USER}/" \
     Dockerfile.hrpsys.in > Dockerfile.hrpsys.${UBUNTU_VERSION}_${CHOREONOID_VERSION}
-${BUILD_CMD} build -f Dockerfile.hrpsys.${UBUNTU_VERSION}_${CHOREONOID_VERSION} \
+${BUILD_CMD} build ${DOCKER_BUILD_OPTION} -f Dockerfile.hrpsys.${UBUNTU_VERSION}_${CHOREONOID_VERSION} \
        --tag=${DOCKER_USER}/hrpsys:${UBUNTU_VERSION}_${CHOREONOID_VERSION} .
 
 
@@ -73,5 +74,5 @@ sed -e "s/@CHOREONOID_VERSION@/${CHOREONOID_VERSION}/" -e "s/@UBUNTU_VERSION@/${
 if [ ! -e ./rtmros_choreonoid ]; then
     git clone https://github.com/start-jsk/rtmros_choreonoid.git
 fi
-${BUILD_CMD} build -f Dockerfile.choreonoidsim.${UBUNTU_VERSION}_${CHOREONOID_VERSION} \
+${BUILD_CMD} build ${DOCKER_BUILD_OPTION} -f Dockerfile.choreonoidsim.${UBUNTU_VERSION}_${CHOREONOID_VERSION} \
        --tag=${CURRENT_NAME}/choreonoidsim:${UBUNTU_VERSION}_${CHOREONOID_VERSION} .
